84a17d334998b1ff2cfe49c7b8bf6ee41a26ac94
JUNGQUI <ljk2518@gmail.com>
2021-01-02 10:24:10 +0900

    Observer pattern 정의
    - class 및 interface
    - test code
    - README 추가

---
+++ src/main/java/com/jk/study/pattern/observer/JKObservable.java
@@ -0,0 +1,24 @@
+package com.jk.study.pattern.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import lombok.Getter;
+
+@Getter
+public class JKObservable extends Observable {
+	private List<Integer> integers;
+
+	public JKObservable () {
+		this.integers = new ArrayList<>();
+	}
+
+	public void setIntegers(int i) {
+		for (int a = 0; a < i; a++) {
+			this.integers.add(a);
+		}
+
+		setChanged();
+		notifyObservers(this.integers);
+	}
+}
---
+++ src/main/java/com/jk/study/pattern/observer/JKObserver.java
@@ -0,0 +1,20 @@
+package com.jk.study.pattern.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+public class JKObserver {
+
+	public List<Integer> getValueByObservable () {
+		List<Integer> integers = new ArrayList<>();
+
+		Observer jkObserver = (o, arg) -> integers.add((Integer) arg);
+
+		JKObservableWithRunnable observable = new JKObservableWithRunnable();
+		observable.addObserver(jkObserver);
+		observable.run();
+
+		return integers;
+	}
+}
