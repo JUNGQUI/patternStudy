bef29b2229627678ab37175275f5307c07f2b417
JUNGQUI <ljk2518@gmail.com>
2021-01-12 21:22:13 +0900

    java study
    - duration 추가

--- src/test/java/com/jk/study/pattern/java/JavaLogicTest.java
+++ src/test/java/com/jk/study/pattern/java/JavaLogicTest.java
@@ -1,5 +1,6 @@
 package com.jk.study.pattern.java;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,18 @@ public class JavaLogicTest {
     LocalDateTime min = max.minusMinutes(30);
 
     Assertions.assertEquals(30, Math.abs(max.getMinute() - min.getMinute()));
+  }
+
+  @Test
+  void durationTest() {
+    Duration duration1 = Duration.ofHours(10);
+    Duration duration2 = Duration.ofHours(10).plusMinutes(30);
+    Duration duration3 = Duration.ofMinutes(630);
+    duration1 = duration1.plusMinutes(30);
+
+    Assertions.assertEquals(duration1, duration2);
+    Assertions.assertEquals(duration1, duration3);
+    Assertions.assertEquals(duration2, duration3);
+    Assertions.assertNotEquals(duration2, duration3.plusMinutes(30));
   }
 }
